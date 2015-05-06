960ea3db25a8c80af4c2f7fdecec94bd1d8c567d
Dima Kogan <dima@secretsauce.net>
2015-05-06 01:24:31 -0700

    initial massage script: gets topology points

---
+++ massage_input.pl
@@ -0,0 +1,171 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use feature ':5.10';
+use JSON;
+
+use PDL;
+use PDL::NiceSlice;
+my $pi = 3.14159265359;
+
+# here I read the json that comes in from the overpass query and massage it to
+# pass to the solver
+#
+# The earth-centered coordinate system has
+#   x points out from lon = 0          (greenwich)
+#   y points out from lon = +90degrees (mongolia)
+#   z points up to the north pole
+my $Rearth                = 6371000.0; # meters
+my $point_spacing_min     = 100.0; # meters
+
+
+my $infile = shift;
+die "Input json must appear on the commandline"         unless defined $infile;
+die "Existing json file must appear on the commandline" unless -e $infile;
+
+my @corners = $infile =~ /(-?[0-9]+(?:\.[0-9]+)?)/g;
+die "Input file must be query_lat0_lon0_lat1_lon1.osm" unless @corners == 4;
+
+# I transform my points to a 2d plane tangent to my Earth sphere (good enough)
+# at the center of the query rectangle. This coordinate system has
+#   x pointing East,  as observed by a viewer at this location
+#   y pointing North, as observed by a viewer at this location
+#   z pointing up,    as observed by a viewer at this location
+
+my @latlon_center = get_center_latlon(@corners);
+my $v_center      = v_from_latlon(@latlon_center);
+my $p_center      = $v_center * $Rearth;
+my $R             = PDL::cat( east_at_latlon (@latlon_center),
+                              north_at_latlon(@latlon_center),
+                              $v_center )->transpose;
+
+# slurp input. Assuming it's not too large
+my $osm = decode_json(`cat $infile`);
+
+
+my $points = pdl[];
+my %nodes;
+for my $elem (@{$osm->{elements}})
+{
+    if($elem->{type} eq 'node')
+    {
+        $nodes{$elem->{id}} = $elem;
+    }
+    elsif($elem->{type} eq 'way')
+    {
+        my $p_last;
+
+        for my $nodeid(@{$elem->{nodes}})
+        {
+            die "Way $elem->{id} references not-yet-seen node $nodeid"
+              unless exists $nodes{$nodeid};
+
+            my $p = $Rearth * v_from_latlon($nodes{$nodeid}{lat}, $nodes{$nodeid}{lon});
+            my $p_mapped = ($p - $p_center) x $R;
+
+            # locally the surface is flat-enough, and I just take the (E,N)
+            # tuple, and ignore the height (deviation from flat)
+            $p = $p_mapped->(0:1);
+            if( defined $p_last )
+            {
+                my $diff_from_prev = $p - $p_last;
+                my $dsq_from_prev  = inner($diff_from_prev, $diff_from_prev);
+                if( $dsq_from_prev > $point_spacing_min * $point_spacing_min )
+                {
+                    my $toprint = $p_last->glue(1,$p);
+                    my $d_from_prev = sqrt($dsq_from_prev);
+                    $diff_from_prev /= $d_from_prev;
+
+                    while( $d_from_prev > $point_spacing_min )
+                    {
+                        $p_last += $diff_from_prev * $point_spacing_min;
+                        $points = $points->glue(1, $p_last);
+
+                        $d_from_prev -= $point_spacing_min;
+                    }
+                }
+            }
+
+            $points = $points->glue(1, $p);
+            $p_last = $p;
+        }
+    }
+}
+
+say $points;
+
+
+
+
+
+
+
+sub get_center_latlon
+{
+    my ($lat0, $lon0, $lat1, $lon1) = @_;
+
+
+    # I have 4 unit vectors (the corners). I want to find the vector that's
+    # their "average" using some metric. Let's minimize the sum angle:
+    #  E = -sum( inner(v, v_i) ) constrained to inner(v,v) = 1
+    # Lagrange multipliers: L = -sum( inner(v, v_i) ) - l*(inner(v,v) - 1)
+    #
+    #  dL/dv = 0 = -sum(v_i) - 2*l*v -> v = k * sum(v_i) ->
+    #       -> v is the normalized average v_i
+    my $v =
+      v_from_latlon( $lat0, $lon0 ) +
+      v_from_latlon( $lat0, $lon1 ) +
+      v_from_latlon( $lat1, $lon0 ) +
+      v_from_latlon( $lat1, $lon1 );
+
+    $v /= sqrt(inner($v,$v));
+
+    return latlon_from_v($v);
+}
+
+sub latlon_from_v
+{
+    my $v = shift;
+
+    my $lat = asin($v->((2)));
+    my $clat = cos($lat);
+    my $lon = atan2( $v->((1)) / $clat, $v->((0)) / $clat);
+
+    return ($lat * 180.0/$pi, $lon * 180.0/$pi);
+}
+
+sub v_from_latlon
+{
+    my ($lat, $lon) = @_;
+
+    my $clon = cos($lon * $pi / 180.0);
+    my $slon = sin($lon * $pi / 180.0);
+    my $clat = cos($lat * $pi / 180.0);
+    my $slat = sin($lat * $pi / 180.0);
+    return pdl( $clon*$clat, $slon*$clat, $slat);
+}
+
+sub north_at_latlon
+{
+    my ($lat, $lon) = @_;
+
+    my $clon = cos($lon * $pi / 180.0);
+    my $slon = sin($lon * $pi / 180.0);
+    my $clat = cos($lat * $pi / 180.0);
+    my $slat = sin($lat * $pi / 180.0);
+
+    return pdl( -$clon*$slat, -$slon*$slat, $clat );
+}
+
+sub east_at_latlon
+{
+    my ($lat, $lon) = @_;
+
+    my $clon = cos($lon * $pi / 180.0);
+    my $slon = sin($lon * $pi / 180.0);
+    my $clat = cos($lat * $pi / 180.0);
+    my $slat = sin($lat * $pi / 180.0);
+
+    return pdl( -$slon, $clon, 0);
+}
